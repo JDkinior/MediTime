58f33ead8c3af2aa7d9658c6833bd4329743695e
jdkinior <invicant@gmail.com>
2025-03-18 20:18:56 -0500

    Update Java and Kotlin compatibility to version 17; upgrade dependencies in pubspec.lock and pubspec.yaml

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,15 +9,14 @@ android {
     ndkVersion = "27.0.12077973"
     namespace = "com.example.meditime"
     compileSdk = 35
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
